701be62e5f3f2f89000f44d28f6acbee697700c8
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-01 06:32:28 +0900

    Time: 49 ms, Memory: 10.9 MB - LeetHub

--- n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -20,6 +20,27 @@ public:
 
 class Solution {
 public:
+    vector<int> postorder(Node* root) {
+        vector<int>ans;
+        vector<Node*>st;
+        if(!root)
+            return ans;
+        st.push_back(root);
+        //   1
+        // 2 5 6
+        //3 4
+        //   6
+        // 3 4 5
+        //1 2
+        while(!st.empty()){
+            Node*n=st.back();st.pop_back();
+            ans.push_back(n->val);//[6,5,4,3,2,1]            
+            for(auto e:n->children)
+                st.push_back(e);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
     void dfs(Node*root,vector<int>&ans){
         if(!root)
             return;
@@ -27,7 +48,7 @@ public:
             dfs(e,ans);
         ans.push_back(root->val);
     }
-    vector<int> postorder(Node* root) {
+    vector<int> postorder_rec(Node* root) {
         vector<int>ans;
         dfs(root,ans);
         return ans;
