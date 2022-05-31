68b1af7cb923754dfea7de387f472d218f0b96fc
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-01 06:17:25 +0900

    Time: 34 ms, Memory: 11 MB - LeetHub

--- n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -20,6 +20,25 @@ public:
 
 class Solution {
 public:
+    vector<int> preorder(Node* root) {
+        vector<int>ans;
+        vector<Node*>st;
+        if(!root)
+            return ans;
+        st.push_back(root);
+        //   1
+        // 2 5 6
+        //3 4
+        while(!st.empty()){
+            Node*c=st.back();st.pop_back();
+            ans.push_back(c->val);
+            reverse(c->children.begin(),c->children.end());
+            for(Node* e:c->children){
+                st.push_back(e);
+            }            
+        }        
+        return ans;
+    }
     void pre(Node* root,vector<int>&ans) {
         if(!root)
             return;
@@ -27,7 +46,7 @@ public:
         for(auto e:root->children)
             pre(e,ans);
     }
-    vector<int> preorder(Node* root) {
+    vector<int> preorder_rec(Node* root) {
         vector<int>ans;
         pre(root,ans);
         return ans;
