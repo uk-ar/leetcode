22667f8ba6d1dd6d54508b3cf30bf5a6c70861bd
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-02 06:17:23 +0900

    Time: 10 ms (10.47%), Space: 8.6 MB (37.99%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& n) {
-        vector<int> ans(n);
-        ans[0]=n[0];
+        //vector<int> ans(n);
+        //ans[0]=n[0];
         for(int i=0;i+1<n.size();i++)
-            ans[i+1]=ans[i]+n[i+1];
-        return ans;
+            n[i+1]+=n[i];
+            //ans[i+1]=ans[i]+n[i+1];
+        return n;
     }
 };
