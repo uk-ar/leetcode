de2f14ef587a6f5d6f4c2553f842889a2277e535
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-02 03:21:48 +0900

    Time: 214 ms (51.20%), Space: 43.1 MB (95.20%) - LeetHub

---
+++ 288-unique-word-abbreviation/288-unique-word-abbreviation.cpp
@@ -0,0 +1,37 @@
+string abbr(string &s){
+    if(s.size()>2){
+        int n=s.size();
+        return s[0]+to_string(n-2)+s[n-1];
+    }else{
+        return s;
+    }
+}
+class ValidWordAbbr {
+public:
+    unordered_map<string,string>m;
+    ValidWordAbbr(vector<string>& d) {
+        for(auto s:d){
+            if(m.count(abbr(s))==0 or m[abbr(s)]==s){
+                m[abbr(s)]=s;
+            }else{
+                m[abbr(s)]="";
+            }            
+        }
+    }
+    
+    bool isUnique(string w) {
+        string a=abbr(w);
+        //cout << w <<":"<< se.count(w) <<":"<< m[a]<<endl;
+        //if(se.count(w)==0)
+        //    return true;
+        if(m.count(a)==0 or m[a]==w)
+            return true;
+        return false;        
+    }
+};
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr* obj = new ValidWordAbbr(dictionary);
+ * bool param_1 = obj->isUnique(word);
+ */
