eedd6f1bb11df0757e2da56e1e1e757e0dd42e1e
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-03 05:11:37 +0900

    Time: 337 ms (21.51%), Space: 49.6 MB (15.74%) - LeetHub

--- 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.cpp
+++ 1168-optimize-water-distribution-in-a-village/1168-optimize-water-distribution-in-a-village.cpp
@@ -21,38 +21,17 @@ public:
         vector<vector<int>>v;
         for(int i=0;i<n;i++){
             v.push_back({we[i],0,i+1});
-            //cout << we[i] << " "<<0<<":"<< i <<endl;
         }
         for(auto &p:pi)
             v.push_back({p[2],p[0],p[1]});
-        sort(v.begin(),v.end());
-        for(auto e:v)
-            cout << e[0] <<":"<<e[1]<<":"<<e[2]<<endl;
-        cout<<endl;
+        sort(v.begin(),v.end());        
         p.assign(n+1,-1);
-        //w.assign(n+1,false);
         int ans=0;
         for(auto e:v){
-            /*if(e.size()==2){
-                int r=root(e[1]);
-                //cout << e[1] << "("<<r<<endl;
-                if(w[r])continue;                
-                ans+=e[0];
-                w[r]=true;
-            }else{*/
-                int r0=root(e[1]),r1=root(e[2]);
-                if(r0==r1)continue;
-                //if(w[r0] and w[r1])continue;                
-                ans+=e[0];
-                /*if(w[r0])
-                    w[r1]=true;
-                if(w[r1])
-                    w[r0]=true;*/
-                unite(r0,r1);
-            //}
-            for(auto t:e)
-                cout << t <<" ";
-            cout <<":"<<ans <<endl;
+            int r0=root(e[1]),r1=root(e[2]);
+            if(r0==r1)continue;
+            ans+=e[0];
+            unite(r0,r1);
         }
         return ans;
     }
