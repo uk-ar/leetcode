d67003b76f6e11af78e9fa30234ec7836e6d66cf
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-08 11:10:36 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
+    bool isP(string s){
+        //string t=s;
+        //reverse(t.begin(),t.end());
+        //return t==s;
+        int l=0,r=s.size()-1;
+        while(l<r and s[l]==s[r])
+            l++,r--;
+        return l>=r;
+    }
     int removePalindromeSub(string s) {
-        string t=s;
-        reverse(t.begin(),t.end());
-        if(t==s)
+        if(isP(s))
             return 1;
         return 2;
     }
