06288e62d6cf1f631de9b260a8df060e88bb6813
yuuki arisawa <yuuki.ari@gmail.com>
2022-06-24 18:32:31 +0900

    Time: 51 ms (78.32%), Space: 28.7 MB (81.82%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -8,19 +8,17 @@ public:
                 return true;
             int n=q.top();q.pop();            
             long d=sum-n;
-            cout <<"0:"<< n <<":"<<d<<":"<<sum<<endl;
-            if(d==0 or n-d<=0)
+            //cout <<"0:"<< n <<":"<<d<<":"<<sum<<endl;
+            if(d==0 or n<=d)
                 return false;
             if(d==1)
                 return true;
             int nex=n%d;
-            if(nex<=0)
+            if(nex==0)
                 return false;
             q.push(nex);
-            //if(n<=0 or d==0)
-            //    return false;
             sum=sum-(n-nex);
-            cout <<"1:"<< n <<":"<<d<<":"<<sum<<endl;
+            //cout <<"1:"<< n <<":"<<d<<":"<<sum<<endl;
         }        
         return false;
     }
