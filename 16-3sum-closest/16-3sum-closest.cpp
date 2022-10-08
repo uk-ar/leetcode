a84aa7d07d5b491dfa21d721395b5545bb1e4995
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-08 11:32:20 +0900

    Time: 1446 ms (5.01%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int t) {
-        int N=nums.size(),ans=INT_MAX;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<N-2;i++){
-            int l=i+1,r=N-1;//O(n**2)     
+    int threeSumClosest(vector<int>& n, int t) {
+        sort(n.begin(),n.end());
+        int N=n.size();
+        int ret=INT_MAX-10000;
+        for(int i=0;i<N;i++){
+            int l=i+1;
+            int r=N-1;
             while(l<r){
-                int s=nums[l]+nums[r]+nums[i];
-                if(s==t)
+                if(abs(ret-t)>abs(n[i]+n[l]+n[r]-t))
+                    ret=n[i]+n[l]+n[r];                
+                if(n[i]+n[l]+n[r]==t)
                     return t;
-                if(ans==INT_MAX or abs(t-ans)>abs(t-s))
-                    ans=s;
-                if(s<t){
+                else if(n[i]+n[l]+n[r]<t)
                     l++;
-                }else{
+                else
                     r--;
-                }
+
+                //cout << i << ":" << l << ":" << r << ":" << ret << endl;
             }
         }
-        return ans;
+        return ret;
     }
 };
