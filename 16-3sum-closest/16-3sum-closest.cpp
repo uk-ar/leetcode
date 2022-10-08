40246805ac4817125aa37307c2c4cccbdcae3d9f
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-08 11:29:30 +0900

    Time: 393 ms (65.96%), Space: 16.3 MB (48.38%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int t) {
+        int N=nums.size(),ans=INT_MAX;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<N-2;i++){
+            int l=i+1,r=N-1;//O(n**2)     
+            while(l<r){
+                int s=nums[l]+nums[r]+nums[i];
+                if(ans==INT_MAX or abs(t-ans)>abs(t-s))
+                    ans=s;
+                if(s<t){
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
