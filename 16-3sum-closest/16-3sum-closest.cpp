3e6730ac69d9c2370adaab6d697a5c71714134b7
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-08 11:31:30 +0900

    Time: 458 ms (53.06%), Space: 16.3 MB (84.20%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,6 +7,8 @@ public:
             int l=i+1,r=N-1;//O(n**2)     
             while(l<r){
                 int s=nums[l]+nums[r]+nums[i];
+                if(s==t)
+                    return t;
                 if(ans==INT_MAX or abs(t-ans)>abs(t-s))
                     ans=s;
                 if(s<t){
