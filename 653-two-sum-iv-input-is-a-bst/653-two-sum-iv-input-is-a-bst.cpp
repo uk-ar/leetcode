a656a7c43fdecd9c8a2037611c8d9d521e1a2c69
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-10 10:59:21 +0900

    Time: 43 ms (92.24%), Space: 36.5 MB (93.16%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,15 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class It{
+    vector<TreeNode*>nodes;
+    bool left;
+    TreeNode*node;
+    TreeNode*cur;
+    public:
+    It(TreeNode*root,bool l){
+        node=root;
+        left=l;
+        next();
+    }
+    TreeNode* next(){
+        while(node){
+            //cout <<"w:"<< node->val << endl;
+            nodes.push_back(node);
+            node=(left ? node->left : node->right);
+        }//[5,3,2]
+        node = nodes.back();nodes.pop_back();//2
+        cur = node;//2
+        //cout << "c:" << cur->val << endl;
+        node = (left ? node->right : node->left);//null   
+        return cur;
+    }
+    TreeNode* current(){
+        return cur;
+    }
+};
 class Solution {
-public:
-    unordered_set<int>s;
+public:    
     bool findTarget(TreeNode* root, int k) {
-        if(!root)
-            return false;
-        if(s.count(k-root->val))
-            return true;
-        s.emplace(root->val);
-        return findTarget(root->left,k) or findTarget(root->right,k);
+        It *l=new It(root,true),*r=new It(root,false);
+        while(l->current() and r->current() and l->current()->val<r->current()->val){  
+            int s=l->current()->val+r->current()->val;
+            //cout << s << endl;
+            if(s==k)
+                return true;
+            if(s<k)
+                l->next();
+            else
+                r->next();
+        }
+        return false;
     }
 };
