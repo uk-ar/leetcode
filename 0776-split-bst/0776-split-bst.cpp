5357ee212fabbce958ba6b56fff2cdf524a1bd85
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-14 06:21:17 +0900

    Time: 11 ms (8.57%), Space: 8.8 MB (55.71%) - LeetHub

--- 0776-split-bst/0776-split-bst.cpp
+++ 0776-split-bst/0776-split-bst.cpp
@@ -12,22 +12,15 @@
 class Solution {
 public:
     vector<TreeNode*> splitBST(TreeNode* root, int target) {
-        vector<TreeNode*>ans(2);
         if(!root)
-            return ans;
-        vector<TreeNode*> l=splitBST(root->left ,target);
-        vector<TreeNode*> r=splitBST(root->right,target);
-        if(root->val<=target){
-            ans[0]=root;//left is all small
-            root->left =l[0];
-            root->right=r[0];
-            ans[1]=r[1];
-        }else{
-            ans[1]=root;//right is all big
-            root->left =l[1];
-            root->right=r[1];
-            ans[0]=l[0];
+            return {nullptr,nullptr};
+        if(root->val>target){
+            vector<TreeNode*> l=splitBST(root->left,target);
+            root->left=l[1];
+            return {l[0],root};
         }
-        return ans;
+        vector<TreeNode*> r=splitBST(root->right,target);
+        root->right=r[0];
+        return {root,r[1]};
     }
 };
