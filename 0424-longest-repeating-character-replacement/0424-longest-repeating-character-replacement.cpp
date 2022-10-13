d28a9e309762945508e52528f45bc23aeafd154a
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-13 22:48:59 +0900

    Time: 14 ms (72.31%), Space: 6.9 MB (97.97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,16 +4,16 @@ public:
         int r=0,l=0,n=s.size(),ans=0,m_count=0;
         vector<int>v(128);
         while(l<n){
-            //auto p=max_elem(v);
             if(r<n and r-l-m_count<=k){//A    
+                ans=max(ans,r-l);
                 m_count=max(++v[s[r]],m_count);
                 r++;
-                if(r-l-m_count<=k)
-                    ans=max(ans,r-l);
             }else{
                 v[s[l]]--;
                 l++;
             }
+            if(r-l-m_count<=k)
+                ans=max(ans,r-l);
         }
         //cout<<endl;
         return ans;
