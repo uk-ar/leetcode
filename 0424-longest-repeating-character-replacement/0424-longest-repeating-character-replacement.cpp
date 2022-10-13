ef6bd4c0fdd9cd16f53289ace53a6ea1fe0e4b9a
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-13 22:46:14 +0900

    Time: 21 ms (52.04%), Space: 7.1 MB (52.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    pair<int,int> max_elem(vector<int>&v){//other,sum
-        int m=0,s=0;
-        for(char c='A';c<='Z';c++){
-            //if(v[c])
-            //    cout << c << v[c]<<endl;
-            m=max(m,v[c]);
-            s+=v[c];
-        }
-        return {s-m,s};
-    }
     int characterReplacement(string s, int k) {
-        int r=0,l=0,n=s.size(),ans=0;
+        int r=0,l=0,n=s.size(),ans=0,m_count=0;
         vector<int>v(128);
         while(l<n){
-            auto p=max_elem(v);
-            //cout <<l <<":"<< r <<":"<< p.first<<":"<<p.second<<endl;
-            if(r<n and p.first<=k){//A    
-                //cout << "a"<<s[r]<<endl;
-                v[s[r]]++;                
+            //auto p=max_elem(v);
+            if(r<n and r-l-m_count<=k){//A    
+                m_count=max(++v[s[r]],m_count);
                 r++;
-                p=max_elem(v);
-                if(p.first<=k)
-                    ans=max(ans,p.second);
-            }else{                
-                //cout << "b"<<endl;
+                if(r-l-m_count<=k)
+                    ans=max(ans,r-l);
+            }else{
                 v[s[l]]--;
                 l++;
             }
