1b7752d6703d14235f4e1dd7e65928764b209bf6
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-14 18:52:07 +0900

    Time: 144 ms (77.11%), Space: 27 MB (30.98%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int longestBeautifulSubstring(string w) {
-        int l=0,r=0,ans=0,n=w.size();
-        unordered_set<char>s;
+        int l=0,r=0,ans=0,n=w.size(),c=0;
+        //unordered_set<char>s;
+        vector<int>s(128);        
         while(l<n){
-            if(s.size()==5)
+            //if(s.size()==5)
+            if(c==5)
                 ans=max(ans,r-l);
             if(r<n and (l==r or w[r-1]<=w[r])){
-                s.emplace(w[r]);
+                //s.emplace(w[r]);
+                if(++s[w[r]]==1)
+                    c++;
                 r++;                
             }else{
-                s.clear();
+                //s.clear();
+                c=0;
+                s['a']=0;
+                s['e']=0;
+                s['i']=0;
+                s['o']=0;
+                s['u']=0;
                 l=r;
             }
         }
