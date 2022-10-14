2abe88f6a2ab7d004951a1543628fe68bb811e7e
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-14 17:56:20 +0900

    Time: 6 ms (52.53%), Space: 6.7 MB (49.52%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int>v(128);
+        for(char &c:s)
+            v[c]++;        
+        bool odd=false;
+        int ans=0;
+        for(int i=0;i<128;i++){
+            if(v[i]%2)
+                odd=true;
+            ans+=(v[i]>>1)<<1;
+        }
+        if(odd)
+            return ans+1;
+        return ans;
+    }
+};
