b42bd890059aa2fd07d23d5b01961c1a057a617b
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-15 20:24:40 +0900

    Time: 125 ms (35.41%), Space: 35.8 MB (29.73%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& n, int g) {
-        vector<int>v;     
-        int c=0;
-        for(int i=0;i<n.size();i++){
-            if(n[i]==0){
-                c++;
-            }else if(i!=0 and n[i-1]!=n[i]){//0->1
-                v.push_back(c);
-                c=0;
-                v.push_back(1);
-            }else{
-                v.push_back(0);
-                v.push_back(1);
-            }
-        }
-        //if(n.back()==1)
-        v.push_back(c);//end with 0
-        /*for(auto e:v){
-            cout << e << " ";
-        }*/
-        //0123456
-        //0111110
-        //^   ^
-        //  ^   ^
-        //cout<<endl;
-        int ans=0;
-        for(int i=0;i+g*2<v.size();i+=2){
-            if(g==0){
-                ans+=v[i]*(v[i]+1)/2;
-            }else{
-                ans+=(v[i]+1)*(v[i+g*2]+1);
-            }
-        }
+        //[1,0,1,0,1]
+        //[1,1,2,2,3]
+        int N=n.size(),ans=0;
+        unordered_map<int,int>m;
+        vector<int>pre(N+1);
+        m[0]++;
+        for(int i=0;i<N;i++){
+            pre[i+1]=pre[i]+n[i];
+            if(m.count(pre[i+1]-g))
+                ans+=m[pre[i+1]-g];
+            m[pre[i+1]]++;
+        }            
         return ans;
     }
 };
