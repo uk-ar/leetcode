64748b841c0fdad2d9a1e37667cadcb9002cecda
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-16 14:03:32 +0900

    Time: 89 ms (52.75%), Space: 28.8 MB (77.12%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& n, int g) {
+        int N=n.size(),i=0,j=0,k=0,ans=0,si=0,sj=0;
+        for(k=0;k<N;k++){
+            //cout << i << ":" << j <<":"<<k<<endl;
+            si+=n[k];
+            sj+=n[k];
+            while(i<N and si>g)
+                si-=n[i++];
+            while(j<k and (sj>g or (sj==g and n[j]==0)))
+                sj-=n[j++];
+            if(si==g)
+                ans+=j-i+1;
+        }
+        return ans;
+    }
+    int numSubarraysWithSum_(vector<int>& n, int g) {
         //[1,0,1,0,1]
         //[1,1,2,2,3]
         int N=n.size(),ans=0;
