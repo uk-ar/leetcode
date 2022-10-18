c5684f0bd9f280aaa4d4108c504a3a20beca04c8
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-18 21:42:13 +0900

    Time: 481 ms (27.42%), Space: 60 MB (85.90%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& n) {
+        int ans=-1;
+        unordered_map<int,int>m;
+        for(auto &e:n){
+            int t=0,v=e;
+            while(v){
+                t+=v%10;
+                v/=10;
+            }
+            //cout << t <<":"<< e <<endl;
+            if(m.count(t)){
+                ans=max(ans,m[t]+e);
+                if(m[t]<e)
+                    m[t]=e;
+            }else{
+                m[t]=e;
+            }            
+        }
+        return ans;
+    }
+};
