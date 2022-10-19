e5a93aaf8a3713493d365171ae573916de26fe3a
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-19 20:38:18 +0900

    Time: 23 ms (95.44%), Space: 11.8 MB (32.34%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int M=1000000000+7;
-    int comb(int n,int k){
-        long ans=1;
-        for(int i=0;i<k;i++){//2/1*1/2 3/1*2/2*1/3
-            ans=(ans*(n-i))%M;
-            ans=(ans/(i+1))%M;
-        }
-        return ans;
-    }
     int countHomogenous(string s) {
         //return comb(4,2);
         long cnt=1,N=s.size(),ans=0;
         char p=s[0];
         for(int i=1;i<N;i++){
+            ans=(ans+cnt)%M;
             if(p==s[i]){
                 cnt++;
             }else{
-                ans=(ans+comb(cnt+1,2))%M;
                 //cout << i << s[i] << cnt << ":"<<ans<<":"<<comb(cnt,2)<<endl;
                 cnt=1;
                 p=s[i];
             }
         }
-        ans=(ans+comb(cnt+1,2))%M;
+        ans=(ans+cnt)%M;
         //cout << cnt << ":"<<ans<<":"<<comb(cnt,2)<<endl;
         return ans;
     }
