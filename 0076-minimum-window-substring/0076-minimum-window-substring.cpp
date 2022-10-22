9fa7fe3cd70242c2d73039867959f30e737e4980
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-22 21:09:53 +0900

    Time: 147 ms (15.60%), Space: 17.2 MB (6.08%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -11,9 +11,10 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char,int>m;
         unordered_set<char>se(t.begin(),t.end());
-        int N=s.size();
+        int N=s.size(),c=0;
         for(int i=0;i<t.size();i++)
-            m[t[i]]++;
+            if(++m[t[i]]==1)
+                c++;
         int l=0,r=0,mi=INT_MAX;
         string ans;
         while(l<N){
@@ -27,11 +28,11 @@ public:
             }
             if(r<N and check(m)>0){//
                 if(se.count(s[r]) and --m[s[r]]==0)
-                    m.erase(s[r]);
+                    c--;
                 r++;
             }else{
-                if(se.count(s[l]) and ++m[s[l]]==0)
-                    m.erase(s[l]);
+                if(se.count(s[l]) and ++m[s[l]]==1)
+                    c++;
                 l++;
             }
         }
