9b2bd2f2fc9f518fbdd26ada20bef4e3b00fa7cf
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-24 20:58:15 +0900

    Time: 389 ms (80.18%), Space: 8.2 MB (18.28%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -6,8 +6,6 @@ public:
     bool dfs(vector<vector<char>>&b, string &w,int r,int c,vector<vector<bool>>&v,int i){
         //cout << r<<":"<<c<<":"<<b[r][c] <<":"<<i<< endl;
         v[r][c]=true;
-        if(w.size()==i)
-            return true;
         if(b[r][c]!=w[i]){
             v[r][c]=false;
             return false;            
@@ -16,7 +14,6 @@ public:
             return true;
         for(int j=0;j<4;j++){
             int nr=r+dr[j],nc=c+dc[j];
-            //cout << r<<":"<<c<<":"<<b[r][c] <<":"<<i<< " "<<nr<<":"<<nc<<endl;
             if(nr<0 or nr>=H or nc<0 or nc>=W or v[nr][nc])
                 continue;
             if(dfs(b,w,nr,nc,v,i+1))
