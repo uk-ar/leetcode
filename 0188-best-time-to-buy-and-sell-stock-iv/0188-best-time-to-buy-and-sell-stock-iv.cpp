f37d9c6a38ffe03f2eac1dd7b45e5ac626f2211f
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-25 20:51:55 +0900

    Time: 188 ms (13.34%), Space: 28.7 MB (5.77%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -4,6 +4,7 @@ public:
         int N=p.size();
         vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(k+1,vector<int>(2,INT_MIN)));
         dp[0][k][0]=0;
+        int ans=0;
         for(int i=0;i<N;i++){
             for(int j=0;j<=k;j++){
                 if(dp[i][j][1]!=INT_MIN){
@@ -14,6 +15,8 @@ public:
                     dp[i+1][j][0]=max(dp[i+1][j][0],dp[i][j][0]);
                 if(dp[i][j][0]!=INT_MIN and j-1>=0)
                     dp[i+1][j-1][1]=max(dp[i+1][j-1][1],dp[i][j][0]-p[i]);
+                ans=max(ans,dp[i+1][j][1]);
+                ans=max(ans,dp[i+1][j][0]);
                 /*dp[i+1][j][true]=dp[i][j][true];
                 if(j+1<k and dp[i][j+1][false]!=INT_MIN)
                     dp[i+1][j][true]=max(dp[i+1][j][true],dp[i][j+1][false]-p[i]);
@@ -21,11 +24,11 @@ public:
                 dp[i+1][j][false]=max(dp[i+1][j][false],dp[i+1][j][true]+p[i]);*/
             }
         }
-        int ans=0;
-        for(int j=0;j<=k;j++){
+        //int ans=0;
+        /*for(int j=0;j<=k;j++){
             ans=max(ans,dp[N][j][false]);
             ans=max(ans,dp[N][j][true]);
-        }
+        }*/
         return ans;
         //return dp[N][0][false];
     }
