90a76cd279619e18a810574908055f4602260817
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-25 22:47:22 +0900

    Time: 1532 ms (5.10%), Space: 72.6 MB (5.28%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    int N;
+    unordered_map<int,unordered_map<int,int>>memo;
+    int dp(vector<vector<int>>& p, int k,int i) {
+        //cout << k << ":"<<i<<endl;
+        if(i==N)
+            return 0;
+        if(memo[k].count(i))
+            return memo[k][i];
+        int ans=0;
+        int t=0;
+        for(int j=0;j<=k and j<=p[i].size();j++){
+            ans=max(ans,dp(p,k-j,i+1)+t);
+            if(j<k and j<p[i].size())
+                t+=p[i][j];
+        }
+        return memo[k][i]=ans;
+    }
     int maxValueOfCoins(vector<vector<int>>& p, int k) {
+        N=p.size();
+        return dp(p,k,0);
+    }
+    int maxValueOfCoins_(vector<vector<int>>& p, int k) {
         int N=p.size(),ans=0;
         vector<vector<int>>dp(N+1,vector<int>(k+1));
         for(int i=0;i<N;i++){
