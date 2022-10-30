da547019d43d5f400e78c328f8c05960b0b4f286
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-30 18:15:36 +0900

    Time: 439 ms (15.90%), Space: 6.8 MB (81.87%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    int vari(string &s,char a,char b) {
-        int ans=0,cur=0;
-        bool has_b=false,first_b=false;
-        for(auto c:s){            
-            cur+=c==a;
-            if(c==b){
-                has_b=true;
-                if(first_b and cur>=0)//2nd b
-                    first_b=false;
-                else if(--cur<0){//3rd b later
-                    first_b=true;
-                    cur=-1;
-                }
+    int vari(string &s,char c0,char c1) {
+        int r0=0,r1=0;
+        for(char c:s)
+            if(c0==c)
+                r0++;
+            else if(c1==c)
+                r1++;
+        int f0=0,f1=0,ans=0;
+        for(char c:s){
+            if(c==c0)
+                f0++;
+            if(c==c1){
+                f1++;
+                r1--;
             }
-            if(has_b)
-                ans=max(ans,cur);
+            if(f0-f1>0 and f1>0)
+                ans=max(ans,f0-f1);            
+            if(f0-f1<0 and r1>0){
+                f0=0;
+                f1=0;
+            }
+            //if(c0=='b' and c1=='a')
+            //    cout << f0 <<":" <<f1 << ":"<<ans<<endl;
         }
         return ans;
     }
     int largestVariance(string s) {
         int ans=0;
-        for(char mi='a';mi<='z';mi++){
-            for(char ma='a';ma<='z';ma++){
-                ans=max(ans,vari(s,mi,ma));
-            }
-        }
-        //"abcde"
-        //"aababbb"
-        return ans;
-    }    
-    int largestVariance_(string s) {
-        int N=s.size(),ans=0;
-        for(int i=0;i<N;i++){
-            unordered_map<char,int>m;
-            map<int,int>f;
-            for(int j=i;j<N;j++){
-                if(--f[m[s[j]]]<=0)
-                    f.erase(m[s[j]]);
-                m[s[j]]++;
-                f[m[s[j]]]++;
-                ans=max(ans,(*f.rbegin()).first-(*f.begin()).first);
+        for(char c0='a';c0<='z';c0++){
+            for(char c1='a';c1<='z';c1++){
+                if(c0==c1)
+                    continue;
+                ans=max(ans,vari(s,c0,c1));
             }
         }
         return ans;
