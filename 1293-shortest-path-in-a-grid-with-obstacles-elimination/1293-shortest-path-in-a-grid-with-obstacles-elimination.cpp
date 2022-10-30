164fee6e0129e4061c85515fb06779c5ff355051
yuuki arisawa <yuuki.ari@gmail.com>
2022-10-30 10:11:28 +0900

    Time: 40 ms (86.37%), Space: 10.8 MB (82.25%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& g, int k) {
+        deque<tuple<int,int,int,int>>q;//r,c,k,d
+        q.push_back({0,0,k,0});
         int H=g.size(),W=g[0].size();
-        deque<tuple<int,int,int,int>>q;
-        q.push_back({0,0,0,k});//r,c,dist,k
+        vector<int>dr={0,-1,0,1};
+        vector<int>dc={-1,0,1,0};
         vector<vector<int>>dist(H,vector<int>(W,INT_MAX));
-        vector<int>dr={0,1,0,-1};
-        vector<int>dc={1,0,-1,0};
+        dist[0][0]=0;
         while(!q.empty()){
-            auto [r,c,d,k]=q.front();q.pop_front();
-            //cout << r <<":"<<c<<":"<<d<<":"<<k<<endl;
-            if(dist[r][c]<=d)
+            auto [r,c,k,d]=q.front();q.pop_front();
+            if(dist[r][c]<d)
                 continue;
             dist[r][c]=d;
-            for(int i=0;i<4;i++){
-                int nr=r+dr[i],nc=c+dc[i];                
-                if(nr<0 or nr>=H or nc<0 or nc>=W)
+            for(int i=0;i<4;i++){                
+                int nr=r+dr[i],nc=c+dc[i];
+                //cout << r <<":"<<c <<" "<<nr <<":"<<nc<<endl;
+                if(nr<0 or nr>=H or nc<0 or nc>=W or dist[nr][nc]<=d+1)
                     continue;
-                if(g[nr][nc]==1 and k==0)
-                    continue;
-                if(g[nr][nc]==1)
-                    q.push_back({nr,nc,d+1,k-1});
-                else
-                    q.push_front({nr,nc,d+1,k});
+                if(g[nr][nc]==1 and k>0){
+                    q.push_back({nr,nc,k-1,d+1});
+                }else if(g[nr][nc]==0){
+                    q.push_front({nr,nc,k,d+1});
+                }
             }
         }
+        /*for(auto &v:dist){
+            for(auto e:v){
+                cout << e <<" ";
+            }
+            cout <<endl;
+        }*/
         if(dist[H-1][W-1]==INT_MAX)
             return -1;
         return dist[H-1][W-1];
