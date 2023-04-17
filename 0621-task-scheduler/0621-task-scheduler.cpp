ba3c5a1f3e77509d7544d0442e9184543ec90e65
yuuki arisawa <yuuki.ari@gmail.com>
2023-04-17 17:56:24 +0900

    Time: 123 ms (29.80%), Space: 44 MB (10.06%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int leastInterval(vector<char>& ta, int n) {
+        unordered_map<char,int>m;
+        for(auto c:ta)
+            m[c]++;
+        priority_queue<int>q;
+        for(auto [_,cnt]:m)
+            q.push(cnt);
+        int ti=0;
+        while(m.size()>0){
+            vector<int>t;
+            for(int i=0;i<n+1;i++){
+                if(q.size()==0 and t.size()==0)
+                    return ti;
+                ti++;
+                if(q.size()==0)
+                    continue;
+                auto e=q.top();q.pop();
+                if(--e>0)
+                    t.push_back(e);
+            }
+            for(auto e:t)
+                q.push(e);
+        }
+        return ti;
+    }
+    int leastInterval1(vector<char>& ta, int n) {
         vector<int>f(128);
         for(auto e:ta)
             f[e]++;
