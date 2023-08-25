a84e7a83f7b8408c98d59573aa01e109d47cd2fa
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-26 07:07:07 +0900

    Time: 8 ms (62.63%), Space: 18.9 MB (9.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -14,19 +14,8 @@ public:
     int maxDepth(TreeNode* root) {
         if(!root)
             return 0;
-        deque<pair<int,TreeNode*>>q;
-        q.push_back({1,root});
-        int ans=1;
-        while(!q.empty()){
-            auto [d,n]=q.back();q.pop_back();
-            //cout << d <<":"<<n->val<<endl;
-            ans=max(ans,d);
-            if(n->left)
-                q.push_back(make_pair(d+1,n->left));
-            if(n->right)
-                q.push_back(make_pair(d+1,n->right));
-            
-        }
-        return ans;
+        int l = maxDepth(root->left );
+        int r = maxDepth(root->right);
+        return max(l,r)+1;
     }
 };
