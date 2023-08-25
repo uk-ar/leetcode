cacaf209bbcdbf3558c61915fa1262766e1fb37f
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-26 07:20:30 +0900

    Time: 87 ms (68.23%), Space: 67.7 MB (36.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int maxSubArray_(vector<int>& n) {
-        int mi=0,pre=0,ret=n[0];
-        for(auto e:n){
-            pre+=e;
-            ret=max(ret,pre-mi);
-            mi=min(mi,pre);
-        }
-        return ret;
-    }
-    int maxSubArray(vector<int>& n) {
-        int ans=INT_MIN,cur=0;
-        for(auto e:n){
-            cur=max(e,cur+e);
-            ans=max(ans,cur);
+    int maxSubArray(vector<int>& nums) {
+        int pre=0,min_pre=0,ans=INT_MIN,N=nums.size();
+        for(int i=0;i<N;i++){
+            pre += nums[i];            
+            ans = max(ans,pre - min_pre);
+            min_pre = min(pre,min_pre);
         }
         return ans;
     }
