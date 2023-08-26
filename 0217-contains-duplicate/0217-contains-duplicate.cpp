95fb54f0312fe687083c25826741f22c25d39db6
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-27 08:02:31 +0900

    Time: 114 ms (63.49%), Space: 71.3 MB (21.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        // hash set O(N) O(N)
         unordered_set<int>s(nums.begin(),nums.end());
-        return s.size()!=nums.size();
+        return s.size() != nums.size();
     }
 };
