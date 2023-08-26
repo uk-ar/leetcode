29ca534d3e89dd6444b0e373224929c95c7f9d73
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-27 07:50:17 +0900

    Time: 4 ms (49.53%), Space: 6.6 MB (78.9%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -6,14 +6,15 @@ public:
         for(char c : s){
             count[c]++;
         }
-        bool has_odd = false;
+        //bool has_odd = false;
         int ans = 0;
         for(int i = 0; i < 128; i++){
             ans += count[i]/2*2;
-            if(count[i]%2){
-                has_odd = true;// 1
+            if(count[i]%2 && ans%2 == 0){
+                ans++;
+                //has_odd = true;// 1
             }
         }
-        return ans + has_odd;
+        return ans;// + has_odd;
     }
 };
