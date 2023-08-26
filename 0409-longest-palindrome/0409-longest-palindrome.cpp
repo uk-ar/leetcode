a7a40304591cf8b013d1153a355ed21c988b468b
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-27 07:45:16 +0900

    Time: 0 ms (100%), Space: 6.7 MB (16.61%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int>v(128);
-        for(char &c:s)
-            v[c]++;        
-        bool odd=false;
-        int ans=0;
-        for(int i=0;i<128;i++){
-            if(v[i]&1)
-                odd=true;
-            ans+=(v[i]>>1)<<1;
+        //counter O(N) O(N)
+        vector<int> count(128);
+        for(char c : s){
+            count[c]++;
         }
-        if(odd)
-            return ans+1;
-        return ans;
+        bool has_odd = false;
+        int ans = 0;
+        for(int i = 0; i < 128; i++){
+            ans += count[i]/2*2;
+            if(count[i]%2){
+                has_odd = true;// 1
+            }
+        }
+        return ans + has_odd;
     }
 };
