d095098a9b64df7ee3b9c5c7fefe4c5a02e30bc8
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-27 10:33:05 +0900

    Time: 18 ms (84.92%), Space: 14 MB (36.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& g,vector<bool>&visit,int i,vector<int>&in) {
-        if(visit[i])
-            return;
-        visit[i]=true;
-        for(auto e : g[i]){
-            in[e]--;
-            if(in[e]==0 && !visit[e])
-                dfs(g,visit,e,in);
-        }
-    }
     bool canFinish(int N, vector<vector<int>>& pre) {
-        vector<int>in(N);
         vector<vector<int>>g(N);
-        for(auto &v:pre){
+        vector<int>in(N);
+        for(auto v : pre){
             in[v[1]]++;
-            g[v[0]].push_back(v[1]);
+            g[v[0]].push_back(v[1]);            
+        }
+        // bfs
+        deque<int>q;
+        for(int i = 0; i < N; i++){
+            if(in[i] == 0){
+                q.push_back(i);
+            }
         }
         vector<bool>visit(N);
-        for(int i = 0; i < N; i++){
-            if(in[i]==0){
-                 dfs(g,visit,i,in);
+        while(q.size() > 0){
+            int e = q.front();q.pop_front();
+            visit[e] = true;
+            for(auto n : g[e]){
+                in[n]--;
+                if(in[n] == 0){
+                    q.push_back(n);
+                }
             }
         }
         for(int i = 0; i < N; i++){
