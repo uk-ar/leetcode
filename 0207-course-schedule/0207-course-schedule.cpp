55622b38d5c41b8a5b192e57503c7f1e9c746797
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-27 10:25:28 +0900

    Time: 20 ms (76.79%), Space: 13.8 MB (49.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool dfs(int numCourses, vector<vector<int>>& g, vector<int>&visit, int s) {
-        if(visit[s]==1)
-            return false;
-        if(visit[s]==2)
-            return true;
-        visit[s] = 1;
-        for(auto n : g[s]){
-            if(!dfs(numCourses,g,visit,n))
-                return false;
+    void dfs(vector<vector<int>>& g,vector<bool>&visit,int i,vector<int>&in) {
+        if(visit[i])
+            return;
+        visit[i]=true;
+        for(auto e : g[i]){
+            in[e]--;
+            if(in[e]==0 && !visit[e])
+                dfs(g,visit,e,in);
         }
-        visit[s] = 2;
-        return true;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // topological sort O(N) O(N)
-        int N= numCourses;
-        vector<int>visit(N);
-        // make graph
+    bool canFinish(int N, vector<vector<int>>& pre) {
+        vector<int>in(N);
         vector<vector<int>>g(N);
-        for(auto pre : prerequisites){
-            g[pre[0]].push_back(pre[1]);
+        for(auto &v:pre){
+            in[v[1]]++;
+            g[v[0]].push_back(v[1]);
         }
-        // dfs
+        vector<bool>visit(N);
         for(int i = 0; i < N; i++){
-            if(visit[i]==0 && !dfs(N,g,visit,i)){
-                return false;
+            if(in[i]==0){
+                 dfs(g,visit,i,in);
             }
         }
         for(int i = 0; i < N; i++){
-            if(!visit[i]){
+            if(!visit[i])
                 return false;
-            }
         }
         return true;
     }
