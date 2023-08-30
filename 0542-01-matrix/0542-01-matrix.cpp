ff5b43195d265dd0af2fcd5d93f25bc838016f90
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-31 05:34:24 +0900

    Time: 72 ms (34.29%), Space: 30.5 MB (52.2%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    int H,W;
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int H = mat.size(),W = mat[0].size();
-        vector<vector<int>>ans(H,vector<int>(W,INT_MAX));
+        H = mat.size(), W = mat[0].size();
+        // bfs
         deque<pair<int,int>>q;
+        vector<vector<int>>ans(H,vector<int>(W,INT_MAX));
         for(int i = 0; i < H; i++){
             for(int j = 0; j < W; j++){
                 if(mat[i][j] == 0){
@@ -12,23 +14,20 @@ public:
                 }
             }
         }
-        vector<int>dr={0,-1,0,1};
-        vector<int>dc={-1,0,1,0};
+        vector<int>dr = {0,-1,0,1};
+        vector<int>dc = {-1,0,1,0};
         while(q.size() > 0){
-            int n = q.size();
-            for(int i = 0; i < n ;i++){
-                auto [r,c]=q.front();q.pop_front();
-                for(int j = 0; j < 4; j++){
-                    int nr = dr[j] + r, nc = dc[j] + c;
-                    if(nr < 0 || nr >=H || nc < 0 || nc >= W){
-                        continue;
-                    }
-                    if(ans[r][c] + 1 >= ans[nr][nc]){
-                        continue;
-                    }
-                    ans[nr][nc] = ans[r][c]+1;
-                    q.push_back({nr,nc});
+            auto [r,c] = q.front();q.pop_front();
+            for(int i = 0; i < 4; i++){
+                int nr = dr[i] + r, nc = dc[i] + c;
+                if(nr < 0 || nr >= H || nc < 0 || nc >= W){
+                    continue;
                 }
+                if(ans[nr][nc] <= ans[r][c]+1){
+                    continue;
+                }
+                ans[nr][nc] = ans[r][c]+1;
+                q.push_back({nr,nc});
             }
         }
         return ans;
