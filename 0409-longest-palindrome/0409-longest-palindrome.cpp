5371cc4d57fabb89f4090d58d938da8cee30a5e2
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-31 05:11:07 +0900

    Time: 4 ms (49.75%), Space: 6.7 MB (47.41%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        //counter O(N) O(N)
+        
         vector<int> count(128);
         for(char c : s){
             count[c]++;
         }
-        //bool has_odd = false;
         int ans = 0;
         for(int i = 0; i < 128; i++){
-            ans += count[i]/2*2;
-            if(count[i]%2 && ans%2 == 0){
+            ans +=count[i]/2*2;
+            if(count[i]%2 && ans%2 != 1){
                 ans++;
-                //has_odd = true;// 1
             }
         }
-        return ans;// + has_odd;
+        return ans;
     }
 };
