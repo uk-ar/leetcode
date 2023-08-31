b13e55396f9da39644a57e89259079f5a34f5081
yuuki arisawa <yuuki.ari@gmail.com>
2023-08-31 21:57:23 +0900

    Time: 15 ms (24.69%), Space: 11.4 MB (15.51%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
-    int N;
-    void white(string &s,int &i){
-        while(i<N and s[i]==' ')
-            i++;
+    int calculate(string s) {
+        int i = 0;
+        string t="(";
+        for(char c : s)
+            if(c!=' ')
+                t.push_back(c);
+        t.push_back(')');
+        return exp(t,i);
     }
     int digit(string &s,int &i){
-        int ans=0;
-        while(i<N and isdigit(s[i])){            
-            ans=ans*10-'0'+s[i];
-            i++;
+        //cout << "dig" << i <<endl;
+        long ans = 0;
+        while(i < s.size() && isdigit(s[i])){
+            ans = ans * 10 + s[i++] - '0';
         }
-        return ans;
+        return ans;        
     }
     int exp(string &s,int &i){
-        vector<int>st;
-        int sign=1;
-        int ans=0;
-        while(i<N){
-            white(s,i);
-            if(s[i]=='('){
-                i++;
-                st.push_back(sign*exp(s,i));
-                sign=1;
-            }else if(s[i]==')'){
-                i++;
-                for(auto e:st)
-                    ans+=e;
-                return sign*ans;
-            }else if(s[i]=='-'){
-                i++;
-                sign=-1;
-            }else if(s[i]=='+'){
-                i++;
-            }else{
-                st.push_back(sign*digit(s,i));
-                sign=1;
-            }
+        //cout << "exp" << i <<endl;
+        int sign = 1;
+        if(s[i]=='-'){
+            sign = -1;
+            i++;
         }
-        for(auto e:st){
-            //cout<<e<<" ";
-            ans+=e;
+        if(s[i]=='('){
+            vector<int>v;
+            i++;
+            //cout << "(" <<endl;
+            while(i < s.size() && s[i]!=')'){
+                v.push_back(exp(s,i));
+            }           
+            //cout << ")" <<endl;
+            i++;
+            int ans = 0;
+            for(auto e : v)
+                ans += e;
+            return sign * ans;
         }
-        //cout<<endl;
-        return sign*ans;
-    }
-    int calculate(string s) {
-        N=s.size();
-        int i=0;
-        return exp(s,i);
+        if(s[i]=='+')
+            i++;
+        return sign * digit(s,i);
     }
 };
