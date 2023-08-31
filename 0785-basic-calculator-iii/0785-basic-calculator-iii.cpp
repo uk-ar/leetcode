567d5150b22435d5a52fb1734488b7e86d0a8697
yuuki arisawa <yuuki.ari@gmail.com>
2023-09-01 05:25:48 +0900

    Time: 4 ms (62.28%), Space: 6.6 MB (80.99%) - LeetHub

---
+++ 0785-basic-calculator-iii/0785-basic-calculator-iii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int N;
+    bool consume(string &s,int &i,char c){
+        if(s[i]==c){
+            i++;
+            return true;
+        }
+        return false;
+    }
+    int exp(string &s,int &i){
+        int acc = mul(s,i);
+        while(i<N){
+            if(consume(s,i,'+')){
+                acc += mul(s,i);
+            }else if(consume(s,i,'-')){
+                acc -= mul(s,i);
+            }else{
+                break;
+            }
+        }
+        return acc;
+    }
+    int mul(string &s,int &i){
+        int acc = primary(s,i);
+        while(i<N){
+            if(consume(s,i,'*')){
+                acc *= primary(s,i);
+            }else if(consume(s,i,'/')){
+                acc /= primary(s,i);
+            }else{
+                break;
+            }
+        }
+        return acc;
+    }
+    int primary(string &s,int &i){
+        int ans = 0;
+        if(consume(s,i,'(')){
+            ans = exp(s,i);
+            i++;//skip ')'
+            return ans;
+        }
+        while(i<N && isdigit(s[i])){
+            ans = ans * 10 -'0' + s[i++];
+        }
+        return ans;
+    }
+    int calculate(string s0) {
+        string s;
+        for(char c : s0){
+            if(c != ' ')
+                s += c;
+        }
+        int i=0;
+        N=s.size();
+        return exp(s,i);
+    }
+};
